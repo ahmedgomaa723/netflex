3b199f7098de9cf9fbd419078eb7deabc662b214
ahmed <ahmedgomaa851@yahoo.com>
2021-04-25 17:07:44 +0300

    final

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function toggleVideo(){
+    'use strick'
+    const trailer = document.querySelector(".trailer");
+    const video = document.querySelector(".video");
+    trailer.classList.toggle("active")
+    video.currentTime = 0;
+    video.pause();
+    
+
+}
+
